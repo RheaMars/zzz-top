d05a265c321ed11d4a1a568bc9a0811cdaff4464
Rhea Marstaller <r.marstaller@gmail.com>
2023-02-25 15:12:10 +0100

    feat: Setup PhpUnit

---
+++ tests/ConverterTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \src\php\Converter
+ */
+final class ConverterTest extends TestCase
+{
+    //TODO Update dummy test with real tests
+    public function testSomething(): void
+    {
+        $this->assertSame("abc", "abc");
+    }
+}
