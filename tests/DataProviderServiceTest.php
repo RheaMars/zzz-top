27c27200ab6ac4532d7e8d73810b260a3b8b5dbe
Rhea Marstaller <r.marstaller@gmail.com>
2023-02-26 18:01:30 +0100

    feat: Extend DataProviderServiceTest.php with test for getData

--- tests/DataProviderServiceTest.php
+++ tests/DataProviderServiceTest.php
@@ -10,7 +10,58 @@ use src\php\DataProviderService;
 final class DataProviderServiceTest extends TestCase
 {
     /**
-     * @dataProvider provideAmbiguousStringData
+     * @dataProvider provideValidAmbiguousStringData
+     */
+    public function testGetData(string $ambiguousString, array $expectedData): void
+    {
+        $service = new DataProviderService();
+
+        $this->assertSame($expectedData, $service->getData($ambiguousString));
+    }
+
+    public function provideValidAmbiguousStringData(): array
+    {
+        return [
+            [
+                '12az',
+                [
+                    [
+                        "lexicographic" => '12.a.z',
+                        "arabic" => '12.1.26',
+                        "greek" => '12.α.κστ'
+                    ]
+                ],
+            ],
+            [
+                '12',
+                [
+                    [
+                    "lexicographic" => '12',
+                    "arabic" => '12',
+                    "greek" => '12'
+                    ]
+                ],
+            ],
+            [
+                '333azb',
+                [
+                    [
+                        "lexicographic" => '333.a.zb',
+                        "arabic" => '333.1.28',
+                        "greek" => '333.α.κη'
+                    ],
+                    [
+                        "lexicographic" => '333.a.z.b',
+                        "arabic" => '333.1.26.2',
+                        "greek" => '333.α.κστ.β'
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideValidAndInvalidAmbiguousStringData
      */
     public function testIsInputValid(string $ambiguousString, bool $expectedResult): void
     {
@@ -25,7 +76,7 @@ final class DataProviderServiceTest extends TestCase
 
     }
 
-    public function provideAmbiguousStringData(): array
+    public function provideValidAndInvalidAmbiguousStringData(): array
     {
         return [
             [
