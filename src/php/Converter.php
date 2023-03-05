cc343c26732378a2a9237e36a1672a20de831937
basil <basil.karadais@cognostics.de>
2023-03-05 11:44:25 +0100

    Factor mapping constants out of Converter

--- src/php/Converter.php
+++ src/php/Converter.php
@@ -7,69 +7,6 @@ use Exception;
 
 final class Converter
 {
-    public const GREEK_MAX_EXPONENT = 6;
-
-    private const GREEK_MAPPING = [
-        1 => 'α',
-        2 => 'β',
-        3 => 'γ',
-        4 => 'δ',
-        5 => 'ε',
-        // 6 => 'ϛ',
-        6 => 'στ',
-        7 => 'ζ',
-        8 => 'η',
-        9 => 'θ',
-        10 => 'ι',
-        20 => 'κ',
-        30 => 'λ',
-        40 => 'μ',
-        50 => 'ν',
-        60 => 'ξ',
-        70 => 'ο',
-        80 => 'π',
-        // 90 => 'πι',
-        90 => 'ϟ',
-        100 => 'ρ',
-        200 => 'σ',
-        300 => 'τ',
-        400 => 'υ',
-        500 => 'φ',
-        600 => 'χ',
-        700 => 'ψ',
-        800 => 'ω',
-        900 => 'ϡ',
-        1000 => '͵α',
-        2000 => '͵β',
-        3000 => '͵γ',
-        4000 => '͵δ',
-        5000 => '͵ε',
-        // 6000 => '͵ϛ',
-        6000 => '͵στ',
-        7000 => '͵ζ',
-        8000 => '͵η',
-        9000 => '͵θ',
-        10000 => '͵ι',
-        20000 => '͵κ',
-        30000 => '͵λ',
-        40000 => '͵μ',
-        50000 => '͵ν',
-        60000 => '͵ξ',
-        70000 => '͵ο',
-        80000 => '͵π',
-        // 90000 => '͵πι',
-        90000 => '͵ϟ',
-        100000 => '͵ρ',
-        200000 => '͵σ',
-        300000 => '͵τ',
-        400000 => '͵υ',
-        500000 => '͵φ',
-        600000 => '͵χ',
-        700000 => '͵ψ',
-        800000 => '͵ω',
-        900000 => '͵ϡ',
-    ];
-
     public function getArabicConversion(array $lexicographicAlternative): array
     {
         $range = range("a", "z");
@@ -103,17 +40,16 @@ final class Converter
         $tailSegments = array_slice($arabicAlternative, 1);
 
         $greekAlternative[] = $headSegment;
-
         foreach ($tailSegments as $tailSegment) {
 
-            $greekMaxNumber = pow(10, self::GREEK_MAX_EXPONENT) - 1;
+            $greekMaxNumber = pow(10, Mapping::GREEK_MAX_EXPONENT) - 1;
             if ($tailSegment > $greekMaxNumber) {
                 throw new Exception("not supported (segment $tailSegment is higher than $greekMaxNumber).");
             }
 
-            $greekAlternativeSegmentNumbers = $this->getPowerOfTenSummands($tailSegment);
+            $greekAlternativeSegmentNumbers = $this->getPowerOfTenSummands($tailSegment, Mapping::GREEK_MAX_EXPONENT);
 
-            $greekAlternativeSegment = $this->mapIntegersByConversionArray($greekAlternativeSegmentNumbers, self::GREEK_MAPPING);
+            $greekAlternativeSegment = $this->mapIntegersByConversionArray($greekAlternativeSegmentNumbers, Mapping::GREEK_NO_STIGMA);
             
             $greekAlternative[] = $greekAlternativeSegment;
                         
@@ -125,12 +61,12 @@ final class Converter
      * Returns the decimal representation summands of an integer as an array of integers.
      * E.g., for the $number 154, the function returns [100, 50, 4].
      */
-    private function getPowerOfTenSummands(int $number): array
+    private function getPowerOfTenSummands(int $number, int $maxExponent): array
     {
         $numbersToMap = [];
             
         $currentRemainder = $number;
-        for ($exponent = (self::GREEK_MAX_EXPONENT - 1); $exponent >= 0; $exponent--) {
+        for ($exponent = ($maxExponent - 1); $exponent >= 0; $exponent--) {
             $quotient = (int)($currentRemainder / pow(10, $exponent));
             $remainder = $currentRemainder % pow(10, $exponent);
 
